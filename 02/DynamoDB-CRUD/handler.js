a39e7a648313d9b205e2ad6171f78d520da046d8
Perumal V <perumalperu1224@gmail.com>
2026-05-22 19:47:08 +0530

    GET, POST Added

--- 02/DynamoDB-CRUD/handler.js
+++ 02/DynamoDB-CRUD/handler.js
@@ -1,3 +1,83 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+
+const {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand
+} = require("@aws-sdk/lib-dynamodb");
+
+const client = new DynamoDBClient({
+  region : "us-east-1"
+});
+
+const dynamoDB = DynamoDBDocumentClient.from(client);
+
+const TABLE_NAME = "Customers";
+
+// GET ALL Customers - ScanCommand()
+module.exports.getCustomers = async()=>{
+    try{
+      const command = new ScanCommand({
+        TableName = TABLE_NAME
+      })
+
+      const response = await dynamoDB.send(command)
+
+      return {
+        statusCode : 200,
+        body : JSON.stringify(response.Items)
+      }
+    } 
+    catch(err){
+      return {
+        statusCode : 500,
+        body : JSON.stringify({
+          error: error.message
+        })
+      }
+    }
+}
+
+// CREATE CUSTOMER - PutCommand
+module.exports.createCustomer = async(event)=>{
+    try{
+      // REQUEST BODY
+      const body = JSON.parse(event.body);
+
+      // NEW OBJECT
+      const customer = {
+        id: uuidv4(),
+        fullName : body.fullName,
+        city : body.city
+      };
+
+      // PUT COMMAND
+      const command = new PutCommand({
+        TableName: TABLE_NAME,
+        Item: customer
+      })
+
+      // SAVE TO DYNAMODB
+      await dynamoDB.send(command);
+
+      return {
+        statusCode: 201,
+        body:JSON.stringify({
+          message: "User Created",
+          data: customer
+        })
+      }
+    }
+    catch(err){
+      return {
+        statusCode : 500,
+        body: JSON.stringify({
+          error: err.message
+        })
+      };
+    }
+};
+
 exports.health = async (event) => {
   return {
     statusCode: 200,
